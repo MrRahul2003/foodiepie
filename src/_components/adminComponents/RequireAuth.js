ac002b4101c39508757a0664db4b66cbefe43bce
MrRahul <rahulsdas2003@gmail.com>
2026-01-10 14:56:17 +0530

    auth update

--- src/_components/adminComponents/RequireAuth.js
+++ src/_components/adminComponents/RequireAuth.js
@@ -7,13 +7,13 @@ import { useRouter } from "next/navigation";
 export default function RequireAdminAuth({ children }) {
   const router = useRouter();
 
-  useEffect(() => {
-    const adminSession = localStorage.getItem("adminSession");
+  // useEffect(() => {
+  //   const adminSession = localStorage.getItem("adminSession");
 
-    if (!adminSession) {
-      router.replace("/admin/auth");
-    }
-  }, []);
+  //   if (!adminSession) {
+  //     router.replace("/admin/auth");
+  //   }
+  // }, []);
 
   return children;
 }
